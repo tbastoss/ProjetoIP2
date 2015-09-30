4610fab8765870d5ac00f7783a70c60e075e5076
otavioalves2 <tavinhojsp@gmail.com>
2015-09-30 18:16:04 -0300

    Classe básica Alimento

--- Dieta&Saude/src/br/com/dieta_saude/java_beans/Alimento.java
+++ Dieta&Saude/src/br/com/dieta_saude/java_beans/Alimento.java
@@ -1,5 +1,20 @@
 package br.com.dieta_saude.java_beans;
 
 public class Alimento {
+	private String nome;
+	private int pontos;
+	
+	public Alimento(String nome, int pontos) {
+		this.nome = nome;
+		this.pontos = pontos;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+	
+	public int getPontos() {
+		return pontos;
+	}
 
 }
