89a79ead1b9befa1afae6bab95e317277c1cad81
Otávio Alves <tavinhojsp@gmail.com>
2015-10-01 16:28:26 -0300

    Classe dieta completa, possivelmente será alterada

--- Dieta&Saude/src/br/com/dieta_saude/java_beans/Dieta.java
+++ Dieta&Saude/src/br/com/dieta_saude/java_beans/Dieta.java
@@ -1,10 +1,21 @@
 package br.com.dieta_saude.java_beans;
 
 public class Dieta {
-	
-	public Dieta()
-	{
-	
+	private int periodo;
+	private Refeicoes[][] refeicoes;
+	private int proxima;
+	public Dieta(int periodo) {
+		this.periodo = periodo;
+		this.refeicoes = new Refeicoes[periodo][6];
+		this.proxima = 0;
 	}
-
+	public void inserirRefeicoes(Refeicoes refeicoes){
+		for(int i=0;i<6;i++){
+			this.refeicoes[this.proxima][i] = refeicoes;
+		}
+		this.proxima = this.proxima + 1;
+		if(this.proxima == periodo){
+			System.out.println("Você deseja mudar sua dieta?");
+		}
+	}
 }
