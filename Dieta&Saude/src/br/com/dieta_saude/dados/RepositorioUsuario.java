718102cfe7ae387ddf42f3b1f90da7ab13a479aa
wallsmanBR <alves_thiagobastos@hotmail.com>
2015-10-01 19:13:30 -0300

    Commit RepositorioUsuario

---
+++ Dieta&Saude/src/br/com/dieta_saude/dados/RepositorioUsuario.java
@@ -0,0 +1,71 @@
+package br.com.dieta_saude.dados;
+
+import br.com.dieta_saude.java_beans.Usuario;
+
+public class RepositorioUsuario {
+	private Usuario[] usuarios;
+	private int proximo;
+	
+	public  RepositorioUsuario (int tamanho){
+		this.usuarios = new Usuario[tamanho];
+		this.proximo = 0;
+		
+	}
+	
+	public void cadastrar (Usuario usuario){
+		this.usuarios[this.proximo] = usuario;
+		this.proximo++;
+	}
+	
+	private int procurarIndice(String nome){
+		int i = 0;
+		boolean achou = false;
+		while ((!achou) && (i < this.proximo)){
+			if (nome.equals(this.usuarios[i].getNome()))
+				achou = true;
+			else 
+				i++;
+		}
+		return i;		
+	}
+	
+	/*
+	 * Nao muda o nome, pois eh o nome de usuário e ja vai ser feita a verificação, pelo controlador de
+	 * usuários, para saber se o nome ja foi usado.  
+	 */
+	public boolean atualizar(String nome, double altura, double peso, int idade, int nivelDeSedentarismo){
+		boolean retorno = false;
+		int i = this.procurarIndice(nome);
+		if (i != this.proximo){
+			this.usuarios[i].setAltura(altura);
+			this.usuarios[i].setPeso(peso);
+			this.usuarios[i].setIdade(idade);
+			this.usuarios[i].setNivelDeSedentarismo(nivelDeSedentarismo);
+			retorno = true;
+		}
+		return retorno;
+	}
+	
+	public Usuario procurar(String nome){
+		int i = this.procurarIndice(nome);
+		Usuario resultado = null;
+		if (i != this.proximo){
+			resultado = this.usuarios[i];			
+		}
+		return resultado;
+		
+	}
+	
+	public boolean remover(String nome){
+		int i = this.procurarIndice(nome);
+		boolean resultado = false;
+		if (i != this.proximo){
+			this.usuarios[i] = this.usuarios[this.proximo - 1];
+			this.usuarios[this.proximo - 1] = null;
+			this.proximo = this.proximo - 1;
+			resultado = true;
+		}
+		return resultado;
+		
+	}
+}
