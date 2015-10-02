9dad5c0219f9df2ad1c78c2a1c905b149d2195ef
cristovao <cristovao@172.16.198.159>
2015-10-01 21:28:07 -0300

    Alteração no construtor de Alimentos. Inclusão de novo pacote: controlador.

---
+++ Dieta&Saude/src/controladorDeUsuario/ControladorDeAlimentos.java
@@ -0,0 +1,44 @@
+package controladorDeUsuario;
+
+import br.com.dieta_saude.dados.RepositorioAlimento;
+import br.com.dieta_saude.dados.RepositorioUsuario;
+import br.com.dieta_saude.java_beans.Alimento;
+import br.com.dieta_saude.java_beans.Usuario;
+
+public class ControladorDeAlimentos {
+	
+	public ControladorDeAlimentos()
+	{
+		
+	}
+	
+	
+	public boolean verificaExistenciaDeAlimento(String nome, RepositorioAlimento repositorio)
+	{
+		boolean resultado = false;
+		if (repositorio.procurar(nome) != null)
+			resultado = true;
+		
+		return resultado;
+	}
+	
+	
+	public boolean adicionaAlimentos(String nome, int pontos, int id, 
+			RepositorioAlimento repositorio)
+	{
+		boolean resultado = false;
+		boolean verificador = verificaExistenciaDeAlimento(nome, repositorio);
+		Alimento teste = new Alimento(nome, pontos, id);
+	
+		if (verificador == false){
+			repositorio.cadastrar(teste);
+			resultado = true;
+		}
+		
+		return resultado;
+	}
+	
+	
+	
+	
+}
