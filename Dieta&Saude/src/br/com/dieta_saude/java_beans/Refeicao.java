9db51f32feebdf045d42c080f5d2733ac1c465ff
wallsmanBR <alves_thiagobastos@hotmail.com>
2015-10-27 20:21:02 -0200

    commit da classe refeicao (com comentarios sobre o que precisa ser pensado, sobre o funcionamento da mesma)

--- Dieta&Saude/src/br/com/dieta_saude/java_beans/Refeicao.java
+++ Dieta&Saude/src/br/com/dieta_saude/java_beans/Refeicao.java
@@ -2,11 +2,19 @@ package br.com.dieta_saude.java_beans;
 
 public class Refeicao {
 	private int id = 0;
-	private Alimento[][] alimentos;
+	private Alimento[] alimentos;
 	private int pontos;
 	
-	public Refeicao (int periodo) {
-		this.alimentos = new Alimento[periodo][];
+	/*
+	 * Pecisa v como vai ficar o funcionamento da classe, ja que foi feita a 
+	 * alteração indicada pelo professor.
+	 * 
+	 * No caso vamos ter um array simples de alimetos e na classe dieta o usuário
+	 * vamos ter um array bidimencional com as dimenções sendo [quantidade de refeições (escolhida pelo usuário)] [periodo da dieta (também escolhido pelo usuário)]
+	 */
+	
+	public Refeicao (int tamanhoRepositorioDeAlimento) {
+		this.alimentos = new Alimento[tamanhoRepositorioDeAlimento];
 		this.pontos = 0;
 		this.id++;
 	}
@@ -17,7 +25,4 @@ public class Refeicao {
 	public void setPontos(int pontos) {
 		this.pontos = pontos;
 	}
-	
-	
-	
 }
